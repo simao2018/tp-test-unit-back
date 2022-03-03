b4e80f3d059b118d0176db30860e7815196ec448
chancel simo <chancel.simo@nextalys.com>
2022-03-03 16:53:15 +0100

    load data from api

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,62 @@
-import { Module } from '@nestjs/common';
+import { HttpModule, HttpService } from '@nestjs/axios';
+import { Inject, Module } from '@nestjs/common';
+import { Repository } from 'typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ProductModule } from './Modules/product.module';
+import { DatabaseModule } from './db-config/database.module';
+import { Product } from './entities/produit.entity';
+import { ProductModule, productProviders } from './Modules/product.module';
 
 @Module({
-  imports: [ProductModule],
+  imports: [
+    HttpModule.registerAsync({
+      useFactory: () => ({
+        timeout: 5000,
+        maxRedirects: 5,
+      })
+    }),
+    ProductModule,
+    DatabaseModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    ...productProviders,
+
+  ],
 })
-export class AppModule { }
+export class AppModule {
+
+  path: string = 'https://rickandmortyapi.com/api/character/';
+  constructor(
+    private httpService: HttpService,
+    @Inject('PRODUCT_REPOSITORY')
+    private productRespository: Repository<Product>
+  ) {
+    console.log('init db');
+    //this.loadDataAndAddFromApi();
+  }
+
+  private async loadDataAndAddFromApi() {
+    const apiProduct = this.httpService.get(this.path).subscribe(x => {
+      console.log("🚀 ~ apiProduct ~ x", x.data.results);
+
+      const products: Product[] = (x.data?.results as any[]).map(y => {
+        return {
+          title: y.name,
+          imageUrl: y.image,
+          price: Math.floor(Math.random() * (500 - 100 + 1)) + 100,
+          qte: Math.floor(Math.random() * (5 - 1 + 1)) + 1,
+        }
+      });
+
+      if (products?.length) {
+        for (const p of products) {
+          this.productRespository.save(p);
+        }
+      }
+
+    })
+  }
+
+}
